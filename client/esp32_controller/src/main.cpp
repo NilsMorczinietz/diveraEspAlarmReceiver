135a6c841b9790cc0f88204b0b5fd9084f6fa0fb
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-12-21 22:30:40 +0100

    controller and worker

---
+++ client/esp32_controller/src/main.cpp
@@ -0,0 +1,81 @@
+#include <WiFi.h>
+#include <ArduinoWebsockets.h>
+#include <Adafruit_NeoPixel.h>
+
+#include "secrets.h"
+#include "settings.h"
+#include "led_control.h"
+
+using namespace websockets;
+
+Adafruit_NeoPixel strip(NUM_MAX_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+WebsocketsClient client;
+
+// Function prototypes
+void setupWiFi();
+void setupWebSocket();
+
+// WebSocket message callback
+void onMessageCallback(WebsocketsMessage message) {
+    Serial.print("Got Message: ");
+    Serial.println(message.data());
+
+    // Turn on all LEDs regardless of the message content
+    turnOnLEDsCount(strip, 40);
+}
+
+// WebSocket event callback
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if (event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("Connection Opened");
+    } else if (event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println("Connection Closed");
+    } else if (event == WebsocketsEvent::GotPing) {
+        Serial.println("Got a Ping!");
+    } else if (event == WebsocketsEvent::GotPong) {
+        Serial.println("Got a Pong!");
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    delay(1000);
+
+    // Setup Wi-Fi and WebSocket
+    setupWiFi();
+    setupWebSocket();
+
+    // Initialize LED strip
+    strip.begin();
+    strip.show();
+}
+
+void loop() {
+    client.poll();  // Continuously check for incoming messages and events
+
+    // Send a Ping every 60 seconds to keep the WebSocket connection alive
+    static unsigned long lastPingTime = 0;
+    if (millis() - lastPingTime >= 60000) {
+        client.ping();
+        Serial.println("Ping sent");
+        lastPingTime = millis();
+    }
+}
+
+void setupWiFi() {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to Wi-Fi...");
+    }
+    Serial.println("Wi-Fi connected!");
+}
+
+void setupWebSocket() {
+    client.onMessage(onMessageCallback);
+    client.onEvent(onEventsCallback);
+    client.connect(WEBSOCKETS_SERVER_HOST);
+
+    client.send("Hello Server");
+    client.ping();
+}
