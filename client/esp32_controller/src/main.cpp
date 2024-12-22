9b2220c6756b2b1938791cdec73136eb84ba7f53
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-12-22 09:32:19 +0100

    multiple workers

--- client/esp32_controller/src/main.cpp
+++ client/esp32_controller/src/main.cpp
@@ -1,34 +1,54 @@
 #include <WiFi.h>
 #include <ArduinoWebsockets.h>
+#include <WiFiUdp.h>
 #include <Adafruit_NeoPixel.h>
 
-#include "secrets.h"
-#include "settings.h"
+#include "secrets.h"  // Contains WIFI_SSID and WIFI_PASSWORD
+#include "settings.h" // Contains WEBSOCKETS_SERVER_HOST
 
 using namespace websockets;
 
 WebsocketsClient client;
+WiFiUDP udp;
+
+// List of worker/slave IP addresses
+const char* workerIPs[] = {"192.168.178.105", "192.168.178.106", "192.168.178.107"};
+const unsigned int udpPort = 4210; // Target port
+const int numWorkers = sizeof(workerIPs) / sizeof(workerIPs[0]);
 
 // Function prototypes
 void setupWiFi();
 void setupWebSocket();
+void reconnectWiFi();
+void reconnectWebSocket();
+void sendUDPMessageToAll(const String& message);
+void sendUDPMessageToSpecific(const String& message, const char* targetIP);
 
 // WebSocket message callback
 void onMessageCallback(WebsocketsMessage message) {
-    Serial.print("Got Message: ");
+    Serial.print("Message received: ");
     Serial.println(message.data());
+
+    // Send UDP message to all workers
+    sendUDPMessageToAll("on");
 }
 
 // WebSocket event callback
 void onEventsCallback(WebsocketsEvent event, String data) {
-    if (event == WebsocketsEvent::ConnectionOpened) {
-        Serial.println("Connection Opened");
-    } else if (event == WebsocketsEvent::ConnectionClosed) {
-        Serial.println("Connection Closed");
-    } else if (event == WebsocketsEvent::GotPing) {
-        Serial.println("Got a Ping!");
-    } else if (event == WebsocketsEvent::GotPong) {
-        Serial.println("Got a Pong!");
+    switch (event) {
+        case WebsocketsEvent::ConnectionOpened:
+            Serial.println("WebSocket connection established.");
+            break;
+        case WebsocketsEvent::ConnectionClosed:
+            Serial.println("WebSocket connection closed. Attempting to reconnect...");
+            reconnectWebSocket();
+            break;
+        case WebsocketsEvent::GotPing:
+            Serial.println("Ping received.");
+            break;
+        case WebsocketsEvent::GotPong:
+            Serial.println("Pong received.");
+            break;
     }
 }
 
@@ -36,37 +56,85 @@ void setup() {
     Serial.begin(9600);
     delay(1000);
 
-    // Setup Wi-Fi and WebSocket
     setupWiFi();
     setupWebSocket();
 }
 
 void loop() {
-    client.poll();  // Continuously check for incoming messages and events
+    // Check and poll WebSocket messages/events
+    if (WiFi.status() == WL_CONNECTED) {
+        if (client.available()) {
+            client.poll();
+        } else {
+            Serial.println("WebSocket not available. Attempting to reconnect...");
+            reconnectWebSocket();
+        }
+    } else {
+        Serial.println("Wi-Fi disconnected. Attempting to reconnect...");
+        reconnectWiFi();
+    }
 
-    // Send a Ping every 60 seconds to keep the WebSocket connection alive
+    // Keep WebSocket connection alive
     static unsigned long lastPingTime = 0;
-    if (millis() - lastPingTime >= 60000) {
+    if (millis() - lastPingTime >= 60000 && client.available()) {
         client.ping();
-        Serial.println("Ping sent");
+        Serial.println("Ping sent.");
         lastPingTime = millis();
     }
 }
 
 void setupWiFi() {
+    Serial.println("Connecting to Wi-Fi...");
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
         delay(1000);
-        Serial.println("Connecting to Wi-Fi...");
+        Serial.print(".");
     }
-    Serial.println("Wi-Fi connected!");
+    Serial.println("\nWi-Fi connected!");
 }
 
 void setupWebSocket() {
+    Serial.println("Connecting to WebSocket...");
     client.onMessage(onMessageCallback);
     client.onEvent(onEventsCallback);
-    client.connect(WEBSOCKETS_SERVER_HOST);
 
-    client.send("Hello Server");
-    client.ping();
+    if (client.connect(WEBSOCKETS_SERVER_HOST)) {
+        Serial.println("WebSocket connected!");
+        client.send("Hello Server");
+        client.ping();
+    } else {
+        Serial.println("WebSocket connection failed.");
+    }
+}
+
+void reconnectWiFi() {
+    if (WiFi.status() != WL_CONNECTED) {
+        WiFi.disconnect();
+        delay(1000);
+        setupWiFi();
+    }
+}
+
+void reconnectWebSocket() {
+    if (!client.available()) {
+        client.close();
+        delay(1000);
+        setupWebSocket();
+    }
+}
+
+void sendUDPMessageToAll(const String& message) {
+    for (int i = 0; i < numWorkers; i++) {
+        udp.beginPacket(workerIPs[i], udpPort);
+        udp.print(message);
+        udp.endPacket();
+        Serial.println("UDP message sent to " + String(workerIPs[i]) + ": " + message);
+    }
+}
+
+void sendUDPMessageToSpecific(const String& message, const char* targetIP) {
+    udp.beginPacket(targetIP, udpPort);
+    udp.print(message);
+    udp.endPacket();
+    Serial.println("UDP message sent to " + String(targetIP) + ": " + message);
 }
