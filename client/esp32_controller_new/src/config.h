44d6e2736db10775c420e188d78233a3c4575cbb
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-12-24 11:23:06 +0100

    updHandler

---
+++ client/esp32_controller_new/src/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Config
+{
+    constexpr const char *WORKER_IPS[] = {"192.168.178.105", "192.168.178.106", "192.168.178.107"};
+    constexpr const unsigned int UDP_PORT = 4210;
+}
