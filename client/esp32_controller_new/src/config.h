459dd3aae0f872d31694ce50f492f30ddafe0176
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-12-25 23:07:45 +0100

    cooldown = 20min

--- client/esp32_controller_new/src/config.h
+++ client/esp32_controller_new/src/config.h
@@ -4,6 +4,6 @@ namespace Config
 {
     constexpr const char* WORKER_IPS[] = {"192.168.178.105", "192.168.178.106", "192.168.178.107"};
     constexpr const unsigned int UDP_PORT = 4210;
-    constexpr const unsigned int ALARM_COOLDOWN_DURATION = 30; //In minutes
+    constexpr const unsigned int ALARM_COOLDOWN_DURATION = 20; //In minutes
     constexpr const unsigned int ALARM_ON_DURATION = 2; //In minutes
 }
