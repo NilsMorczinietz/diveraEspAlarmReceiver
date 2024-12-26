447814e5a4bec507b492e8cc97831d4af5e2d82c
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-12-26 17:28:29 +0100

    correct ipv4

--- client/esp32_controller/src/config.h
+++ client/esp32_controller/src/config.h
@@ -2,7 +2,7 @@
 
 namespace Config
 {
-    constexpr const char* WORKER_IPS[] = {"192.168.178.105", "192.168.178.106", "192.168.178.107"};
+    constexpr const char* WORKER_IPS[] = {"192.168.178.105", "192.168.178.158", "192.168.178.35"};
     constexpr const unsigned int UDP_PORT = 4210;
     constexpr const unsigned int ALARM_COOLDOWN_DURATION = 20; //In minutes
     constexpr const unsigned int ALARM_ON_DURATION = 2; //In minutes
