88cd78063aa79d308c5762698094049c79a770f2
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-12-27 12:36:04 +0100

    new PROBE_ON_DURATION and sketch

--- client/esp32_controller/src/config.h
+++ client/esp32_controller/src/config.h
@@ -6,6 +6,6 @@ namespace Config
     constexpr const unsigned int UDP_PORT = 4210;
     constexpr const unsigned int ALARM_COOLDOWN_DURATION = 20; //In minutes
     constexpr const unsigned int ALARM_ON_DURATION = 2; //In minutes
-    constexpr const unsigned int PROBE_ON_DURATION = 2; //In minutes
+    constexpr const unsigned int PROBE_ON_DURATION = 1; //In minutes
     constexpr const unsigned int UDP_TIMEOUT = 1; //In minutes
 }
